6b84d351aa3876c8c1d5470e857e2f493b19faea
wwww.anechka@gmail.com <wwww.anechka@gmail.com>
2021-10-13 12:09:51 +0300

    Add some functions

--- example.js
+++ example.js
@@ -4,3 +4,22 @@ $(function(){
         alert('Мир jQuery');
     });
 });
+
+$(function(){
+  $('button').bind('click', function(){
+    alert('Я сдеяль!');
+  })
+})
+
+// $('button').bind('click', function(e){
+//         console.log('Событие инициировал элемент ' + e.target.tagName);
+//         console.log('Событие обрабатывается для элемента ' + e.currentTarget.tagName);
+//     });
+//     $('div').bind('click', function(e){
+//         console.log('Событие инициировал элемент ' + e.target.tagName);
+//         console.log('Событие обрабатывается для элемента ' + e.currentTarget.tagName);
+//     });
+//     $('body').bind('click', function(e){
+//         console.log('Событие инициировал элемент ' + e.target.tagName);
+//         console.log('Событие обрабатывается для элемента ' + e.currentTarget.tagName);
+//     });
--- table.js
+++ table.js
@@ -1,7 +1,4 @@
 jQuery(document).ready(function(){
   $("#tab").css('background-color', 'silver');
-})
-
-$(document).ready(function(){
   $(".tabhead").css('background-color', 'gray');
 })
